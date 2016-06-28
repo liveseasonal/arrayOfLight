572c45cca9d89c4e6b963e9426e607811911beec
liveseasonal <matt@liveseasonal.com>
2016-06-28 02:33:00 +0000

    First JS function

---
+++ arrayoflight.js
@@ -0,0 +1,20 @@
+function arrayoflight(x) {
+  answer = []
+  for (i = 0; i <= x; i++)
+    answer.push(i);
+    // answer[i] = i;
+  return answer;
+};
+
+console.log(arrayoflight(10));
+
+
+function arrayOfLight(x) {
+  var array = [];
+  for ( i = 0; i <= x; i++) {
+    array.push(i);
+  }
+  return array;
+}
+
+console.log(arrayOfLight(10));
